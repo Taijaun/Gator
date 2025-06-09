62dfa537a2a46ddc327c6e0990ed856ca25e18f3
Taijaun Pitt <taijaun@hotmail.co.uk>
2025-06-10 00:43:57 +0100

    created functio nto fetch feed and return it as a single object

---
+++ src/rss/feed.ts
@@ -0,0 +1,52 @@
+import { XMLParser } from "fast-xml-parser";
+import { RSSFeed, RSSItem } from "./rssfeed";
+
+export async function fetchFeed(feedURL: string) {
+    const response = await fetch(feedURL, {
+        headers: {
+            "User-Agent": "gator"
+        }
+    });
+
+    const textResponse = await response.text();
+    const xmlParser = new XMLParser();
+
+    const responseObj = xmlParser.parse(textResponse);
+
+    if (!responseObj.channel){
+        throw new Error("Channel does not exist")
+    }
+
+    const channel = responseObj.channel;
+
+    if (!("title" in channel) && !("link" in channel) && !("description"in channel)){
+        throw new Error("fields not present in channel");
+    }
+
+    const channelTitle = channel.title;
+    const channelLink = channel.link;
+    const channelDescription = channel.description;
+    let channelItem = Array.isArray(channel.item) ? channel.item : [];
+    const validRssItems: RSSItem[] = [];
+
+    for (let field of channelItem){
+        if (("title" in field) && ("link" in field) && ("description" in field) && ("pubDate" in field)){
+            validRssItems.push(field);
+        }
+    }
+
+    const newChannel = {
+        title: channelTitle,
+        link: channelLink,
+        description: channelDescription,
+        item: validRssItems
+    };
+
+    const finalObj: RSSFeed = {
+        channel: newChannel
+    }
+
+    return finalObj;
+
+
+}
