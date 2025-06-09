6611db7e9c62d00f85522d69260223c9ac7753e5
Taijaun Pitt <taijaun@hotmail.co.uk>
2025-06-09 23:45:46 +0100

    Added rss types

---
+++ src/rss/rssfeed.ts
@@ -0,0 +1,15 @@
+export type RSSFeed = {
+    channel: {
+        title: string;
+        link: string;
+        description: string;
+        item: RSSItem[];
+    };
+};
+
+type RSSItem = {
+    title: string;
+    link: string;
+    description: string;
+    pubDate: string;
+}
