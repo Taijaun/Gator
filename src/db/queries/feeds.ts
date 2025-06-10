1b4acd9537bffd1540db4837d4d870d3cd4e0fde
Taijaun Pitt <taijaun@hotmail.co.uk>
2025-06-10 11:21:27 +0100

    refactored addfeed to directly take the logged in users id

--- src/db/queries/feeds.ts
+++ src/db/queries/feeds.ts
@@ -1,15 +1,27 @@
+import { readConfig } from "src/config";
 import { db } from "../index";
 import { feeds } from "../schema";
 import { eq } from "drizzle-orm";
+import { getUserByName } from "./users";
 
-export async function addFeed(name: string, url: string, userId: string){
-    const feedLength = await getFeedByUrl(url)
+export async function addFeed(name: string, url: string){
+    const feedLength = await getFeedByUrl(url);
+    const loggedInUsername = readConfig().currentUserName;
+    let loggedInUserId;
+
+    if (typeof(loggedInUsername) === "string") {
+        const userObj = await getUserByName(loggedInUsername);
+        loggedInUserId = userObj.id
+    } else {
+        return;
+    }
+
     if (feedLength > 0) {
         console.log("This feed already exists in the db");
         return;
     }
 
-    const [result] = await db.insert(feeds).values({name: name, url: url, userId: userId}).returning();
+    const [result] = await db.insert(feeds).values({name: name, url: url, userId: loggedInUserId}).returning();
     return result;
 
 }
