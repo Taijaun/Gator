1ed4a918211335fa0e77a63128591e5a8b14beae
Taijaun Pitt <taijaun@hotmail.co.uk>
2025-06-16 16:01:30 +0100

    Updated agg command to automatically retrieve feeds at given user time interval

--- src/commands/commandAgg.ts
+++ src/commands/commandAgg.ts
@@ -1,10 +1,47 @@
+import { scrapeFeeds } from "src/db/queries/feeds";
 import { fetchFeed } from "src/rss/feed";
 
 export async function handlerAgg(cmdName: string, ...args: string[]){
-    const url = "https://www.wagslane.dev/index.xml";
-    const feed = await fetchFeed(url);
+    const timeBetweenReqsStr = args[0];
+    const timebetweenReqs = parseDuration(timeBetweenReqsStr);
 
-    for (const title of feed.channel.item){
-        console.log(title);
-    };
+    console.log(`Collecting feeds every ${timeBetweenReqsStr}`);
+    scrapeFeeds()
+
+    const interval = setInterval(() => {
+            scrapeFeeds().catch(console.error);
+        }, timebetweenReqs);
+
+    await new Promise<void>((resolve) => {
+        process.on("SIGINT", () => {
+            console.log("Shutting down feed aggregator...");
+            clearInterval(interval);
+            resolve();
+        });
+    });
+}
+
+function parseDuration(durationStr: string): number {
+    const regex = /^(\d+)(ms|s|m|h)$/;
+    const match = durationStr.match(regex);
+
+    if (!match) {
+        throw new Error("Invalid furation format");
+    }
+
+    const value = parseInt(match[1], 10);
+    const unit = match[2];
+
+    switch (unit) {
+        case "ms":
+            return value;
+        case "s":
+            return value * 1000;
+        case "m":
+            return value * 60 * 1000;
+        case "h":
+            return value * 60 * 60 * 1000;
+        default:
+            throw new Error("Invalid time unit");
+    }
 }
