d7295f300ad49b9f3c3b2eed683e9654d1305fdf
Taijaun Pitt <taijaun@hotmail.co.uk>
2025-06-17 11:40:47 +0100

    Created getpostsforuser function

--- src/db/queries/posts.ts
+++ src/db/queries/posts.ts
@@ -1,6 +1,6 @@
 import { db } from "../index";
-import { eq, and, sql } from "drizzle-orm";
-import { Post, posts } from "../schema";
+import { eq, and, sql, desc } from "drizzle-orm";
+import { feedFollows, Post, posts } from "../schema";
 import { getSingleFeedByUrl } from "./feeds";
 
 export async function createPost(post: Post){
@@ -12,4 +12,19 @@ export async function createPost(post: Post){
     }
 
     console.log("Post added")
+}
+
+
+export async function getPostsForUser(limit: number, userId: string){
+    const result = db.select({
+        id: posts.id,
+        title: posts.title,
+    })
+    .from(posts).innerJoin(feedFollows, eq(posts.feedId, feedFollows.feedId))
+    .where(eq(feedFollows.userId, userId))
+    .orderBy(desc(posts.publishedAt))
+    .limit(limit)
+
+    return result;
+    
 }
